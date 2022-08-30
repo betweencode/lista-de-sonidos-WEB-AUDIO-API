de1116bd95cda446789f60702069713b6e542162
samv <mariscal@gmail.com>
2022-08-29 21:22:35 -0500

    final del video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { sonido,listas } from './archivo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'listasonidos';
+  
+  public listas:Array<sonido> = listas;
+  private audio = new Audio();
+
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+      
+    
+  }
+
+
+  public reproduciondosonido(item:sonido){
+      this.audio.src = item.playlist;
+      
+      
+      if(item.reproduciendo){
+          this.audio.pause();
+          item.reproduciendo = false;
+      }else{
+        for(let item of this.listas){
+          item.reproduciendo = false;
+      }
+        this.audio.play();
+        item.reproduciendo = true;
+        setTimeout(() => {
+          item.reproduciendo = false;
+        },item.tiempo);
+      }
+      
+
+      // let audiobyHtml = document.createElement("audio");
+      // audiobyHtml.src = item.playlist;
+      // audiobyHtml.play();
+  }
+
+
+  public eliminandoElemento(indice:number){
+     this.listas.splice(indice,1);
+  }
+
+  
+
   
 }
--- src/app/archivo.ts
+++ src/app/archivo.ts
@@ -3,14 +3,15 @@ export interface sonido{
     subtitulo:string,
     playlist:string,
     imagen:string,
-    reproduciendo:boolean
+    reproduciendo:boolean,
+    tiempo:number
 }
 
 export const listas:Array<sonido>=[
-    {titulo:"Explosión",subtitulo:"explotando ando",imagen:'assets/imagenes/explot.webp',playlist:'assets/sonidos/explote.wav',reproduciendo:false},
-    {titulo:"Pajaros",subtitulo:"Son pajaraos",imagen:'assets/imagenes/pajaro.jpg',playlist:'assets/sonidos/birds.wav',reproduciendo:false},
-    {titulo:"Cines",subtitulo:"Andamos en el cine",imagen:'assets/imagenes/cine.jpg',playlist:'assets/sonidos/cine.wav',reproduciendo:false},
-    {titulo:"Juegos",subtitulo:"Esto es un juego",imagen:'assets/imagenes/juegos.jpg',playlist:'assets/sonidos/juegos.wav',reproduciendo:false},
-    {titulo:"Notificaciones",subtitulo:"Te llegó la notificación",imagen:'assets/imagenes/notifications.webp',playlist:'assets/sonidos/notificacion.wav',reproduciendo:false},
-    {titulo:"Rocket",subtitulo:"Sonido rocket",imagen:'assets/imagenes/rocket.jpg',playlist:'assets/sonidos/rocket.wav',reproduciendo:false},
-    {titulo:"Es noche",subtitulo:"Sonido noche",imagen:'assets/imagenes/noche.jpg',playlist:'assets/sonidos/noche.wav',reproduciendo:false}];
+    {titulo:"Explosión",subtitulo:"explotando ando",imagen:'assets/imagenes/explot.webp',playlist:'assets/sonidos/explote.wav',reproduciendo:false,tiempo:4000},
+    {titulo:"Pajaros",subtitulo:"Son pajaraos",imagen:'assets/imagenes/pajaro.jpeg',playlist:'assets/sonidos/birds.wav',reproduciendo:false,tiempo:4000},
+    {titulo:"Cines",subtitulo:"Andamos en el cine",imagen:'assets/imagenes/cine.jpeg',playlist:'assets/sonidos/cine.wav',reproduciendo:false,tiempo:4000},
+    {titulo:"Juegos",subtitulo:"Esto es un juego",imagen:'assets/imagenes/juegos.jpeg',playlist:'assets/sonidos/juegos.wav',reproduciendo:false,tiempo:4000},
+    {titulo:"Notificaciones",subtitulo:"Te llegó la notificación",imagen:'assets/imagenes/notifications.webp',playlist:'assets/sonidos/notificacion.wav',reproduciendo:false,tiempo:4000},
+    {titulo:"Rocket",subtitulo:"Sonido rocket",imagen:'assets/imagenes/rocket.png',playlist:'assets/sonidos/rocket.wav',reproduciendo:false,tiempo:4000},
+    {titulo:"Es noche",subtitulo:"Sonido noche",imagen:'assets/imagenes/noche.jpeg',playlist:'assets/sonidos/noche.wav',reproduciendo:false,tiempo:4000}];
